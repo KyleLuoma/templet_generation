5c45249b55ce2ccb84618daf204b316216d8443e
kyle.r.luoma <kyle.r.luoma.mil@mail.mil>
2019-11-08 14:37:54 -0500

    Added comments to functions

--- main.py
+++ main.py
@@ -59,9 +59,24 @@ def prepare_fms_file():
         """Calculate baseline templet quantity for each row"""
         fms_uic.at[row.Index, 'TEMPLET_QTY'] = math.ceil(row.AUTHMIL * TEMPLET_PERCENT)
 
+""" 
+Relies on global dataframe HD_map
+HD_map implements HQDA G-3 direction for UIC to HDUIC mapping nomenclature
+Creates a UIC -> HSDUIC code map 
+"""
 def prepare_HD_map():
     HD_map.set_index("UIC", drop = True, inplace = True)
 
+""" 
+Relies on global dataframe aos_uic  and HD_map
+This file is a composite export of three root nodes in AOS:
+    Army commands
+    Army Staff
+    Secretary of the Army
+It is acquired by right clicking on the node in AOS and exporting the UIC tree
+for each of these nodes. The files must be merged together in excel and the header
+and footer and classification banners must be stripped. Then convert to .csv
+"""
 def prepare_aos_uic_file():
     aos_uic["UIC_PUD"] = ""
     aos_uic["EXPECTED_HDUIC"] = ""
@@ -71,19 +86,36 @@ def prepare_aos_uic_file():
         if (HD_map.index.isin([row.UIC[4:6]]).any()):
             aos_uic.at[row.Index, 'EXPECTED_HDUIC'] = row.UIC[0:4] + HD_map.loc[row.UIC[4:6]].HDUIC
         
-        
+"""
+Relies on global dataframe drrsa_uic
+File provided by Jin Kang-Mo 
+Export of all UICs for COMPO 1 stored in DRRS-A
+"""
 def prepare_drrsa_uic_file():
     drrsa_uic["UIC_PUD"] = drrsa_uic.UIC
     
     for row in drrsa_uic.itertuples():
         drrsa_uic.at[row.Index, 'UIC_PUD'] = row.UIC[0:4]
 
+"""
+Relies on global dataframe drrsa_uic
+Can only be run after running prepare_drrsa_uic_file() function
+Args: PUD_ONLY (boolean) to indicate that a four character PUD is desired
+Returns: Series consisting of only HSUICs in the DRRS-A file, full HSUIC if PUD_ONLY is False
+"""
 def make_drrsa_hduic_index(PUD_ONLY):
     if(PUD_ONLY):
         return drrsa_uic.UIC_PUD.where(drrsa_uic.HSUIC_FLAG == "Y").dropna()
     else:
         return drrsa_uic.UIC.where(drrsa_uic.HSUIC_FLAG == "Y").dropna()
 
+"""
+Relies on global dataframs aos_uic and series hduic_index
+Checks if the expected DUIC in AOS exists in the DRRS-A HDUIC index processed
+in the make_drrsa_hduic_index(False) function
+Must run after make_drrsa_hduic_index()
+Returns: dataframe consisting of the AOS UICs where its HDUIC does not exist in DRRSA
+"""
 def aos_drrsa_hduic_check():
     aos_uic['HAS_DUIC'] = False
     aos_uic.HAS_DUIC = aos_uic.EXPECTED_HDUIC.isin(hduic_index)
