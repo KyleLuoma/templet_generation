5cfd45c5981482886523aa2f9026c2612d92f7d8
kyle.r.luoma <kyle.r.luoma.mil@mail.mil>
2020-06-04 19:50:11 -0400

    changed filepath to master file. Updated file names to reflect new AOS data load

--- load_data.py
+++ load_data.py
@@ -8,15 +8,23 @@ Sources include DRRSA UIC file and AOS tree export
 
 import pandas as pd
 
+DRRSA = "DRRSA_Data_20200114.csv"
+RCMS = "rcms_UIC_assigned_excess_04302020.csv"
+EMILPO = "EMILPO_ASSIGNMENTS_3-3-20.csv"
+AOS_W00EFF = "W00EFF C2 UIC TREE 6-4-2021.xlsx"
+AOS_WARCFF = "WARCFF C2 UIC TREE 6-4-2021.xlsx"
+AOS_WSTAFF = "WSTAFF C2 UIC TREE 6-4-2021.xlsx"
+FILEPATH = "X:/AOS/master_files/"
+
 """ Retrieve the DRRSA UIC / Location file """
 def load_drrsa_file():
-    return pd.read_csv("./data/drrsa/DRRSA_Data_20200114.csv")
+    return pd.read_csv("./data/drrsa/" + DRRSA)
 
 """ Retrieve seperate AOS UIC export files and consolidate """
 def load_aos_file():
     return (
             pd.read_excel(
-            "./data/uic_trees/W00EFF C2 UIC TREE 4-6-2021.xlsx",
+            FILEPATH + "aos/uic_tree/" + AOS_W00EFF,
             header = 2,
             dtype = {
                     "HOGEO" : str,
@@ -34,7 +42,7 @@ def load_aos_file():
             skipfooter = 1            
             ).fillna("").append(
             pd.read_excel(
-            "./data/uic_trees/WARCFF C2 UIC TREE 4-6-2021.xlsx",
+            FILEPATH + "aos/uic_tree/" + AOS_WARCFF,
             header = 2,
             dtype = {
                     "HOGEO" : str,
@@ -52,7 +60,7 @@ def load_aos_file():
             skipfooter = 1
             ).fillna("")).append(
             pd.read_excel(
-            "./data/uic_trees/WSTAFF C2 UIC TREE 4-6-2021.xlsx",
+            FILEPATH + "aos/uic_tree/" + AOS_WSTAFF,
             header = 2,
             dtype = {
                     "HOGEO" : str,
@@ -74,30 +82,30 @@ def load_aos_file():
 
 """ Retrieve FMS UIC Rollup with Military Authorizations """
 def load_fms_file():
-    return pd.read_csv("./data/fms/FY21_AC_UIC_and_SUBCO_UIC_Rollup.csv").append(
-            pd.read_csv("./data/fms/FY21_RC_UIC_and_SUBCO_UIC_Rollup.csv")
+    return pd.read_csv(FILEPATH + "fms/FY21_AC_UIC_and_SUBCO_UIC_Rollup.csv").append(
+            pd.read_csv(FILEPATH + "fms/FY21_RC_UIC_and_SUBCO_UIC_Rollup.csv")
             )
 
 """ Retrieve previous FY FMS UIC Rollup with Military Authorizations"""
 def load_prev_fms_file():
-    return pd.read_csv("./data/fms/FY20_AC_UIC_and_SUBCO_UIC_Rollup.csv").append(
-            pd.read_csv("./data/fms/FY20_RC_UIC_and_SUBCO_UIC_Rollup.csv")
+    return pd.read_csv(FILEPATH + "fms/FY20_AC_UIC_and_SUBCO_UIC_Rollup.csv").append(
+            pd.read_csv(FILEPATH + "fms/FY20_RC_UIC_and_SUBCO_UIC_Rollup.csv")
             )
 
 """ Retrieve FMS LDUIC Rollup with Military Authorizations """
 def load_fms_lduic_file():
-    return pd.read_csv("./data/fms/FY21 AC LDUIC Rollup.csv").append(
-            pd.read_csv("./data/fms/FY21 RC LDUIC Rollup.csv")
+    return pd.read_csv(FILEPATH + "fms/FY21 AC LDUIC Rollup.csv").append(
+            pd.read_csv(FILEPATH + "fms/FY21 RC LDUIC Rollup.csv")
             )
 
 """ Retrieve AA / SUBCODE to HD map """
 def load_HD_map():
-    return pd.read_csv("./data/maps_xwalks/UIC_HD_MAP.csv")
+    return pd.read_csv(FILEPATH + "uic_hd_map/UIC_HD_MAP.csv")
 
 """ Retrieve EMILPO assignment rollup file """
 #UIC,PARENT_UIC,CMD,IN_AUTH,ASSIGNED,EXCESS
 def load_emilpo():
-    emilpo = pd.read_csv("./data/emilpo/EMILPO_ASSIGNMENTS_3-3-20.csv")
+    emilpo = pd.read_csv(FILEPATH + "emilpo/" + EMILPO)
     emilpo_rollup = (
             emilpo[["UIC_CD", "PARENT_UIC_CD", "STRUC_CMD_CD", 
                     "SSN_MASK_HASH", "PARNO", "MIL_POSN_RPT_NR"]]
@@ -120,7 +128,7 @@ def load_emilpo():
             )
     
 def load_rcms(file_format = "RAVALA"):
-    file = "./data/rcms/rcms_assigned_excess_01302020.csv"
+    file = FILEPATH + "rcmsr/uic_assigned/" + RCMS
     rcms = pd.read_csv(file)
     if(str.upper(file_format) == "RAVALA"):
         rcms = rcms[["UIC", "Unit Assigned Strength", "Excess"]].rename(
@@ -131,9 +139,13 @@ def load_rcms(file_format = "RAVALA"):
                 )
         rcms["CMD"] = "AR"
         rcms["EXCESS"] = rcms.apply(
-                    lambda row: row["EXCESS"] if row["EXCESS"] >= 0 else 0,
-                    axis = 1
-                )
+            lambda row: (
+                row["EXCESS"] 
+                if row["EXCESS"] >= 0 
+                else 0
+            ),
+            axis = 1
+        )
         rcms["IN_AUTH"] = rcms["ASSIGNED"] - rcms["EXCESS"]
     return rcms
 
@@ -142,6 +154,6 @@ def load_rcms(file_format = "RAVALA"):
 """ Retrieve AOS UIC OUID Crosswalk """
 def load_uic_ouids():
     return pd.read_csv(
-            "./data/maps_xwalks/OUID_UIC_FY21.csv",
+            FILEPATH + "xwalks/OUID_UIC_FY21.csv",
             dtype = {"UIC" : str, "OUID" : str}
             )
